55708ce79b32dc1461e90ba6bb81c8d5457ea263
Jonathan Smith <jonathan@hustle.com>
2018-03-03 13:44:12 -0800

    Add plotting of equation and cost to gradient descent

--- gradient.py
+++ gradient.py
@@ -1,6 +1,9 @@
 # gradient.py
 # Functions related to getting gradients
 
+import matplotlib.pyplot as plt
+import numpy as np
+
 def squared_error(a, b):
   return (b - a)*(b - a)
 
@@ -30,11 +33,11 @@ def mse_cost_2d(theta, training_set):
   for se in squared_errors:
     sum_squared_errors += se
 
+  # Use 2*m to make derivative simpler later
   return 1 / (2*m) * sum_squared_errors 
 
 def array_abs_diff(a, b):
   arr = [abs(a_i - b_i) for a_i, b_i in zip(a, b)]
-  print('Diff: ', arr)
   return arr
 
 def array_all_lt(a, b):
@@ -43,25 +46,22 @@ def array_all_lt(a, b):
       return False
   return True
 
-
-
-def gradient_descent_mse_2d(training_set, cost_function, learning_rate=0.75, convergence_diff=[0.01,0.01]):
+def gradient_descent_mse_2d(training_set, learning_rate=0.05, convergence_diff=[0.01,0.01], plotting=False):
   """Returns the feature vector theta that minimizes the cost function
-  Partial derivative based on using mean squared error cost function
+  Partial derivative from mean squared error cost function
   
   Args:
     training_set (array[float, float]): set of points
-    cost_function ( function([float, float], array[float, float]) ): uses feature hypothesis
       and training set to determine cost of hypothesis
     learning_rate (float): how fast we change our hypothesis
-    convergance_diff (float): difference to check before assuming convergence
+    convergence_diff (float): difference to check before assuming convergence
     
   Returns:
     [float, float]: ideal features to describe training set
   """
 
   # Randomly set initial hypothesis
-  hypothesis_theta = [0.0, 0.0]
+  hypothesis_theta = [0.0, 1.0]
 
   # so that we don't converge immediately
   last_theta = [
@@ -73,8 +73,22 @@ def gradient_descent_mse_2d(training_set, cost_function, learning_rate=0.75, con
   training_xs = [point[0] for point in training_set]
   training_ys = [point[1] for point in training_set]
 
+  costs = []
+
   while not array_all_lt( array_abs_diff(hypothesis_theta, last_theta), convergence_diff ):
-    print("Hypothesis:", hypothesis_theta)
+    if (plotting):
+      # plot the line created by theta features
+      
+      x = np.linspace(-15, 15, 100)
+      y = hypothesis_theta[0] + hypothesis_theta[1]*x
+      plt.plot(x, y)
+    cost = mse_cost_2d(hypothesis_theta, training_set)
+    costs.append(cost)
+    
+    print("Hypothesis: ", hypothesis_theta)
+    print("  Last Hyp: ", last_theta)
+    print("  Difference:", array_abs_diff(hypothesis_theta, last_theta))
+    print("  Cost: ", cost)
     last_theta = hypothesis_theta
 
     # this is hard-coded for now based on using the mse_cost_2d function
@@ -92,13 +106,28 @@ def gradient_descent_mse_2d(training_set, cost_function, learning_rate=0.75, con
         hypothesis_theta[0]*sum(map(lambda x: x*x, training_xs)) +
         sum(map(lambda x,y: x*y, training_xs, training_ys))) / m,
     ]
+    ht = hypothesis_theta
 
     new_hypothesis = [
       hypothesis_theta[0] - learning_rate * partial_deriv[0],
       hypothesis_theta[1] - learning_rate * partial_deriv[1],
     ]
-
     hypothesis_theta = new_hypothesis
+  
+  if (plotting):
+    # This plots what our line has been doing through all iterations
+    # Ideally there will be one final line going through the training set
+    plt.plot(training_xs, training_ys, 'ro')
+    plt.title('Iterations of theta vs. training set')
+    plt.show()
+
+    # This plots our cost function. Should converge to zero
+    plt.plot(costs, 'ro')
+    plt.title('Cost vs. Iterations of Gradient Descent')
+    plt.ylabel('J(theta) cost')
+    plt.xlabel('Iteration')
+    plt.show()
+  return hypothesis_theta
 
 
 
--- test_gradient.py
+++ test_gradient.py
@@ -14,5 +14,18 @@ class TestGradientFunctions(unittest.TestCase):
       ]
     self.assertEqual(gradient.mse_cost_2d(theta, training_set), 0.25)
 
+  def test_gradient_descent_mse_2d(self):
+    training_set = [
+        [0, 0],
+        [1, 2],
+        [2, 4],
+        [3, 6],
+      ] 
+    expected_theta = [0, 2]
+    cd = 0.01
+    estimated_theta = gradient.gradient_descent_mse_2d(training_set, learning_rate=0.1, convergence_diff=[cd, cd], plotting=True)
+    self.assertTrue(abs(expected_theta[0] - estimated_theta[0]) < cd)
+    self.assertTrue(abs(expected_theta[1] - estimated_theta[1]) < cd)
+
 if __name__ == '__main__':
   unittest.main();
